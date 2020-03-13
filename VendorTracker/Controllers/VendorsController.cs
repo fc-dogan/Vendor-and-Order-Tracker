102718e8c1ac9ea022509e69521560b81568ef8a
Fatma <fceyda@gmail.com>
2020-03-13 10:33:09 -0700

    create VendorsController.cs

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -1,13 +1,31 @@
-  
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using VendorTracker.Models;
-using System.Collections.Generic;
 
 namespace VendorTracker.Controllers
 {
   public class VendorsController : Controller
   {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.VendorsList;
+      return View(allVendors);
+    }
 
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name)
+    {
+      Vendor newVendor = new Vendor(name);
+      return RedirectToAction("Index");
+    }
 
   }
 } 
