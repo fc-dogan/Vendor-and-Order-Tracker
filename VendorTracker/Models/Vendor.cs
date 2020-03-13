9e6d06bd4fbaa7aebb79ba7aeecaab67dc8d74de
Fatma <fceyda@gmail.com>
2020-03-13 09:36:12 -0700

    create Models/Vendor.cs

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -5,6 +5,48 @@ namespace VendorTracker.Models
 {
   public class Vendor
   {
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public List<Order> OrdersList {get; set;} 
+    public int Id {get; set;}
+    public static int CurrentId {get; set;} = 0;
+
+    public static List<Vendor> VendorsList {get; set;} = new List<Vendor> {};
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      OrdersList = new List <Order>{};
+      IncrementId();
+      Id = CurrentId;
+      VendorsList.Add(this);
+    }
+
+    public static void IncrementId()
+    {
+      CurrentId ++;
+    }    
+
+    public void AddOrder(Order order)
+    {
+      OrdersList.Add(order);
+    }
+
+    public static Vendor Find(int id)
+    {
+      for(int i = 0; i < VendorsList.Count; i++ )
+      {
+        if (VendorsList[i] is Vendor)
+        {
+          if(VendorsList[i].Id == id)
+          {
+            return Vendors[i];
+          }
+        }
+      }
+      return null;
+    }    
 
   }
 }  
